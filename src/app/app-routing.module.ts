9c17e46367ffd2191f25142fc55e0173e1974a6e
Abdulla thanseeh <thanseehabdulla@gmail.com>
2018-10-02 20:49:55 +0530

    packages

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from '@angular/router';
+import {AdminComponent} from './admin/admin.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {LoginComponent} from './login/login.component';
+
+const routes: Routes = [
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: 'admin', component: AdminComponent },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'login', component: LoginComponent }
+];
+
+
+@NgModule({
+  imports: [
+    CommonModule, RouterModule.forRoot(routes)
+  ],
+    exports: [ RouterModule ],
+  declarations: []
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    DashboardComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+      FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
